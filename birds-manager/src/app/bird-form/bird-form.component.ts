07526803c19512b48728ea5a159625e1f0eec0b9
Hosana Gomes <hosannagomes@hotmail.com>
2023-09-07 16:58:34 -0300

    delete unecessary log

--- birds-manager/src/app/bird-form/bird-form.component.ts
+++ birds-manager/src/app/bird-form/bird-form.component.ts
@@ -9,7 +9,6 @@ export class BirdFormComponent {
     isFemale: boolean = false;
 
     onChangeSex(event: Event) {
-      console.log((event.target as HTMLInputElement))
       let element = (event.target as HTMLInputElement)
       this.isFemale = element.id == "birdSexFemale" && element.checked
     }
