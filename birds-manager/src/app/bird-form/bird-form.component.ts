80f5d07e46371782b40befb1a02ea0eacb05848e
Hosana Gomes <hosannagomes@hotmail.com>
2023-09-07 16:55:20 -0300

    Binding first egg relating to sex

--- birds-manager/src/app/bird-form/bird-form.component.ts
+++ birds-manager/src/app/bird-form/bird-form.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./bird-form.component.css']
 })
 export class BirdFormComponent {
+    isFemale: boolean = false;
 
+    onChangeSex(event: Event) {
+      console.log((event.target as HTMLInputElement))
+      let element = (event.target as HTMLInputElement)
+      this.isFemale = element.id == "birdSexFemale" && element.checked
+    }
 }
